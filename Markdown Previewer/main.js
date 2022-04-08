15d1e071e1016a448a3873cffcc116850c45b6f2
Isaac Cantu <isaac.cantu707@gmail.com>
2022-04-08 10:36:32 -0500

    no output still

--- Markdown Previewer/main.js
+++ Markdown Previewer/main.js
@@ -2,30 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            markdown: placeholder,
-        }
-
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(event){
-        event.this.setState()
-    }
-
     render(){
         return(
-            <div>
-                <h1>This is React</h1>
-            </div>
-        );
+            <h1>hello world</h1>
+        )
     }
 }
 
 
 
-const placeholder = '# Welcome to Markdown Previewer!'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"))
