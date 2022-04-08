fa41a9c86f04f6b6d9293c3603bcab4573aa34b0
Isaac Cantu <isaac.cantu707@gmail.com>
2022-04-08 05:30:17 -0500

    need to remake with react

--- Markdown Previewer/main.js
+++ Markdown Previewer/main.js
@@ -1,6 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 class App extends React.Component{
     constructor(props){
-        super(props)
+        super(props);
+        this.state = {
+            markdown: placeholder,
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        event.this.setState()
+    }
+
+    render(){
+        return(
+            <h2>This is React!</h2>
+        )
     }
 }
 
